f6a8f30af20c03662e7a78c3de9e4f8bb12078b9
aanjeneykodesia-web <aanjeneykodesia@gmail.com>
2025-12-28 19:32:20 +0530

    Add files via upload

--- FINAL_PRODUCTION_BACKEND/src/main/java/com/production/payment/PaymentController.java
+++ FINAL_PRODUCTION_BACKEND/src/main/java/com/production/payment/PaymentController.java
@@ -1,1 +1,27 @@
+package com.production.payment;
 
+import com.razorpay.*;
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+
+ private RazorpayClient client;
+
+ public PaymentController() throws RazorpayException {
+  client = new RazorpayClient(
+   System.getenv("RAZORPAY_KEY_ID"),
+   System.getenv("RAZORPAY_SECRET")
+  );
+ }
+
+ @PostMapping("/create-order")
+ public String createOrder(@RequestParam int amount) throws RazorpayException {
+  JSONObject options = new JSONObject();
+  options.put("amount", amount * 100);
+  options.put("currency", "INR");
+  return client.orders.create(options).toString();
+ }
+}
